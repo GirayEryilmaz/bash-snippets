5ed332f24c182c3dbea2f00dbfca7e728d97e9e1
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2023-08-04 12:27:23 -0400

    Update run_py_amulet_step1.sh

--- amulet/run_py_amulet_step1.sh
+++ amulet/run_py_amulet_step1.sh
@@ -6,6 +6,12 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem=16GB
 
+
+# Amulet needs some data from per_barcode_metrics, I copied the needed columns (barcode and is_cell) to singlecell_atac.csv. 
+# is_cell needs to be renamed to is__cell_barcode. Amulet as of now requires this. 
+# We use the python version because java version can not handle too many cells (integer overflows).
+# Before starting Amulet, we need to remove known doublets and negatives found by demux step. That is why we also keep the barcode column which we will use for that. 
+
 source /home/${USER}/.bashrc
 source activate base
 
