ff2665ce7af3e286df2c3afaae6ca0778a6d7849
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2023-08-04 14:58:47 -0400

    Update run_py_amulet_step1.sh

--- amulet/run_py_amulet_step1.sh
+++ amulet/run_py_amulet_step1.sh
@@ -8,7 +8,8 @@
 
 
 # Amulet needs some data from per_barcode_metrics, I copied the needed columns (barcode and is_cell) to singlecell_atac.csv. 
-# is_cell needs to be renamed to is__cell_barcode. Amulet as of now requires this. 
+# The original amulet script needed is_cell to be renamed to is__cell_barcode. You can change the one occurence of is__cell_barcode to is_cell in FragmentFileOverlapCounter.py
+# or rename that column to is__cell_barcode
 # We use the python version because java version can not handle too many cells (integer overflows).
 # Before starting Amulet, we need to remove known doublets and negatives found by demux step. That is why we also keep the barcode column which we will use for that. 
 
@@ -17,8 +18,13 @@ source activate base
 
 echo "Running Overlap Counter..."
 
-OUTS="/path/outs"
-AMULET="/path/AMULET-v1.1"
+OUTS="/projects/ucar-lab/giray/DOGMA-seq/out-S-2-100kcells/outs"
+AMULET="/projects/ucar-lab/giray/AMULET-v1.1"
+
+
+cut -d, -f1,4 "$OUTS"/per_barcode_metrics.csv > "$OUTS"/singlecell_atac.csv
+
+mkdir "$OUTS"/amulet
 
 python "$AMULET"/FragmentFileOverlapCounter.py \
 	"$OUTS"/atac_fragments.tsv.gz \
